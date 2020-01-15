1f2c67056e5fb61e50990aaa321e509d98a03bc3
Sanil Lad <sanil@gettimely.com>
2020-01-16 11:06:05 +1300

    Remove Interpolation errors on terraform

--- terraform/timely-supplier.tf
+++ terraform/timely-supplier.tf
@@ -8,8 +8,8 @@ resource "azurerm_resource_group" "rg_timely_supplier" {
 # The App Service plan
 resource "azurerm_app_service_plan" "appserviceplan_supplier" {
   name                = "timely-internal-apps"
-  resource_group_name = "${azurerm_resource_group.rg_timely_supplier.name}"
-  location            = "${azurerm_resource_group.rg_timely_supplier.location}"
+  resource_group_name = azurerm_resource_group.rg_timely_supplier.name
+  location            = azurerm_resource_group.rg_timely_supplier.location
 
   sku {
     tier     = "Standard"
@@ -19,25 +19,25 @@ resource "azurerm_app_service_plan" "appserviceplan_supplier" {
 }
 
 resource "azurerm_app_service" "app_supplier_prod" {
-  name                    = "timely-supplier"
-  resource_group_name     = "${azurerm_resource_group.rg_timely_supplier.name}"
-  location                = "${azurerm_resource_group.rg_timely_supplier.location}"
-  app_service_plan_id     = "${azurerm_app_service_plan.appserviceplan_supplier.id}"
+  name                = "timely-supplier"
+  resource_group_name = azurerm_resource_group.rg_timely_supplier.name
+  location            = azurerm_resource_group.rg_timely_supplier.location
+  app_service_plan_id = azurerm_app_service_plan.appserviceplan_supplier.id
 
   https_only = true
 
 }
 
 resource "azurerm_app_service" "app_supplier_api_prod" {
-  name                    = "timely-supplier-api"
-  resource_group_name     = "${azurerm_resource_group.rg_timely_supplier.name}"
-  location                = "${azurerm_resource_group.rg_timely_supplier.location}"
-  app_service_plan_id     = "${azurerm_app_service_plan.appserviceplan_supplier.id}"
+  name                = "timely-supplier-api"
+  resource_group_name = azurerm_resource_group.rg_timely_supplier.name
+  location            = azurerm_resource_group.rg_timely_supplier.location
+  app_service_plan_id = azurerm_app_service_plan.appserviceplan_supplier.id
 
   https_only = true
-  
-  auth_settings{
-    enabled = true
+
+  auth_settings {
+    enabled         = true
     runtime_version = "2.2"
   }
 
