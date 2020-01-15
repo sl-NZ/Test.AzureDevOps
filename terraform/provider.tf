44a615429bd7788d428670b695fb689c41dc89a8
Sanil Lad <sanil@gettimely.com>
2020-01-16 10:38:48 +1300

    commented out backend provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,16 +1,15 @@
 terraform {
   required_version = ">= 0.12.6"
 
-  backend "azurerm" {
-    #     #  resource_group_name  = "storage-account-rg"
-    #     #  storage_account_name  = "terraform"
-    #     #  container_name        = "terraform-container"
-    #     #  key                   = "supplier.tfstate"
-    #     #  access_key            = "enter your blob storage key here"
-  }
+  # backend "azurerm" {
+  #          resource_group_name  = "storage-account-rg"
+  #          storage_account_name  = "terraform"
+  #          container_name        = "terraform-container"
+  #          key                   = "supplier.tfstate"
+  #          access_key            = "enter your blob storage key here"
+  # }
 }
+
 provider "azurerm" {
   version = ">= 1.38.0"
 }
-
-
