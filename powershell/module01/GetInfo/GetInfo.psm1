598e0aa8bc10b3f70a1e8d75f003ce8d01c3635d
Sanil Lad <sanil@gettimely.com>
2021-01-23 23:31:39 +1300

    test

--- powershell/module01/GetInfo/GetInfo.psm1
+++
@@ -1,8 +0,0 @@
-function Get-Info {
-    param($ComputerName)
-    Get-WmiObject -ComputerName $ComputerName -Class Win32_BIOS
-}
-Function Get-Hello {
-    Write-Host "Hello from my Azure DevOps Services Package."
-}
-
